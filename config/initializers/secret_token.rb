c7cab645769337da7820d7b7189a6874623033ea
Kovács Barna <coolreef@gmail.com>
2013-03-20 22:04:04 +0100

    fordítások mennek

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picurbolt::Application.config.secret_token = 'ccae8e47775565288205971e872ebb615b8699454cbd4c60b0dd8ed22d46db8482c6f280efb6d0756790851541243c37c1f0f4eecadb4bac4ff05625849eb42f'
