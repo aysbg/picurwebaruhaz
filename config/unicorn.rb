bc126bc5fb65a7509e267d1724046f6f49c274d2
gwuix2 <coolreef@gmail.com>
2013-03-22 20:23:50 +0100

    bruhuuhu

--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root = "/home/gwuix2/picurwebaruhaz/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicron.log"
-
-listen "/tmp/unicorn.blog.sock"
-worker_processes 2
-timeout 30
