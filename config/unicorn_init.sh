74bbbea17fd545c0c3aa3d543d582ec74523b7a1
gwuix2 <coolreef@gmail.com>
2013-03-22 20:41:13 +0100

    manemtudom

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/gwuix2/picurwebaruhaz/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deployer
+AS_USER=gwuix2
 set -u
 
 OLD_PIN="$PID.oldbin"
