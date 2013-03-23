821ae8889ef0b9689ecf4e9b5572ab75f9f98c35
gwuix2 <coolreef@gmail.com>
2013-03-23 20:26:17 +0100

    foreman

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,17 +11,39 @@ set :application, "picurwebaruhaz"
 set :user, "gwuix2"
 set :group, 'www-data'
 set :rails_env, 'production'
-
-
 set :scm, "git"
 set :repository,  "git://github.com/gwuix2/picurwebaruhaz.git"
 set :branch, "master"
 set :deploy_to, "/home/#{user}/#{application}"
 
-
 default_run_options[:pty] = true 
+ssh_options[:forward_agent] = true
 
-after "deploy:restart", "deploy:cleanup"
+
+namespace :foreman do
+  desc "Export the Procfile to Bluepill's .pill script"
+  task :export, :roles => :app do
+    run "cd #{current_path} && bundle exec foreman export bluepill /home/gwuix2/#{application}/shared/config"
+    sudo "bluepill load /home/gwuix2/#{application}/shared/config/#{application}.pill"
+  end
+
+  desc "Start the application services"
+  task :start, :roles => :app do
+    sudo "bluepill #{application} start"
+  end
+
+  desc "Stop the application services"
+  task :stop, :roles => :app do
+    sudo "bluepill #{application} stop"
+  end
+
+  desc "Restart the application services"
+  task :restart, :roles => :app do
+    sudo "bluepill #{application} restart"
+  end
+end
+
+
 
  namespace :deploy do
    task :start do ; end
@@ -29,24 +51,13 @@ after "deploy:restart", "deploy:cleanup"
    task :restart, :roles => :app, :except => { :no_release => true } do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
+   %w[start stop restart].each do |command|
+   desc "#{command} unicorn server"
+   task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+   end
  end
 
-
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
-
-
-
-  namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
-    end
-  end
-  
 
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
@@ -56,8 +67,6 @@ after "deploy", "deploy:cleanup" # keep only the last 5 releases
     puts "Now edit the config files in #{shared_path}."
   end
   
-  after "deploy:setup", "deploy:setup_config"
-
   
   task :create_symlinks do
     run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
@@ -75,8 +84,6 @@ after "deploy", "deploy:cleanup" # keep only the last 5 releases
     run "ln -nfs #{shared_path}/config/Procfile #{release_path}/Procfile"
     run "ln -nfs #{shared_path}/config/.foreman #{release_path}/.foreman"
   end
-  
-  after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
@@ -110,5 +117,12 @@ namespace :unicorn do
 end
 
 before "deploy:assets:precompile", "deploy:symlink_shared"
-
+after "deploy:restart", "deploy:cleanup"
 after "deploy:restart", "unicorn:restart"
+after "deploy:setup", "deploy:setup_config"
+after "deploy:finalize_update", "deploy:symlink_config"
+before 'deploy:start', 'foreman:export'
+after 'deploy:start', 'foreman:start'
+
+before 'deploy:restart', 'foreman:export'
+after 'deploy:restart', 'foreman:restart'
