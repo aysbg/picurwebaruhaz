365aa117d8e841fbf704c65c7160a854e744fb32
gwuix2 <coolreef@gmail.com>
2013-03-23 17:56:06 +0100

    procifle error

--- config/deploy.rb
+++ config/deploy.rb
@@ -111,6 +111,24 @@ namespace :deploy do
   before "deploy", "deploy:check_revision"
 end
 
+namespace :unicorn do
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, except: { no_release: true } do
+    run "kill -s USR2 `cat /tmp/unicorn.[application's name].pid`"
+  end
+ 
+  desc "Start unicorn"
+  task :start, except: { no_release: true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+ 
+  desc "Stop unicorn"
+  task :stop, except: { no_release: true } do
+    run "kill -s QUIT `cat /tmp/unicorn.[application's name].pid`"
+  end
+end
+
+after "deploy:restart", "unicorn:restart"
 
 before 'deploy:assets:precompile', 'deploy:symlink_shared'
 
