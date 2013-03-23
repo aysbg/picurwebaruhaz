661cbc6327744e09db6316e7bf004e36855afb6a
gwuix2 <coolreef@gmail.com>
2013-03-23 15:51:43 +0100

    :)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,24 @@
 require "rvm/capistrano"
 require "bundler/capistrano"
-require 'capistrano/nginx/tasks'
+
+set :rvm_ruby_string, 'ruby-1.9.3-p392'
+set :rvm_type, :user
+
+server "72.14.183.209", :web, :app, :db, primary: true
 
 set :application, "picurwebaruhaz"
+set :user, "gwuix2"
 
 set :scm, "git"
 set :repository,  "git://github.com/gwuix2/picurwebaruhaz.git"
 set :branch, "master"
-set :user, "gwuix2"
 set :deploy_to, "/home/#{user}/#{application}"
+
 
 default_run_options[:pty] = true 
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "198.211.117.84"                          # Your HTTP server, Apache/etc
-role :app, "198.211.117.84"                          # This may be the same as your `Web` server
-role :db,  "198.211.117.84", :primary => true # This is where Rails migrations will run
-
-
-# if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-
-# If you are using Passenger mod_rails uncomment this:
  namespace :deploy do
    task :start do ; end
    task :stop do ; end
@@ -57,11 +48,21 @@ namespace :deploy do
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
   end
+  
   after "deploy:setup", "deploy:setup_config"
 
+  
+  task :create_symlinks do
+    run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+    run " -nfs #{shared_path}/config/ldap.yml #{release_path}/config/ldap.yml"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
+  
+  
   after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
