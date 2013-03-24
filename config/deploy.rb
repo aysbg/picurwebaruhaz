54c55dd01228ceb1ef853d0f8b28db2a495893f1
gwuix2 <coolreef@gmail.com>
2013-03-24 09:15:58 +0100

    ssl fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,12 @@ namespace :deploy do
    task :restart, :roles => :app, :except => { :no_release => true } do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
+    %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+      task command, roles: :app, except: {no_release: true} do
+        run "/etc/init.d/unicorn_#{application} #{command}"
+      end
+    end
 
 
   task :setup_config, roles: :app do
